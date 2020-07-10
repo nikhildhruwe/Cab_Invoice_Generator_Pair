f9841daa43e2cc8ebf4a1cf18d260109ae111100
nikhildhruwe <nick.dhruwe@gmail.com>
2020-07-10 20:38:51 +0530

    Added: Test case for minimum fare

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -1,12 +1,9 @@
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KM = 1;
     private static final double COST_PER_KM = 10;
-    private static final double MINIMUM_FARE = 5;
 
     public double calculateFare(double distance , int time) {
             double totalFare = distance * MINIMUM_COST_PER_KM * time * COST_PER_KM;
-            if ( totalFare < MINIMUM_FARE)
-                return MINIMUM_FARE;
             return totalFare;
     }
 }
--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -10,4 +10,13 @@ public class InvoiceTest {
         double fare = invoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare , 0.0);
     }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnMinFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5, fare , 0.0);
+    }
 }
