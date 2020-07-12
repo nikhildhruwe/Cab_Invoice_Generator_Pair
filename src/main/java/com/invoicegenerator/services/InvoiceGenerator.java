f56eb9d7530c21e5728c9d6de8529182e87324af
nikhildhruwe <nick.dhruwe@gmail.com>
2020-07-13 00:36:31 +0530

    Refactored: Proper line spaces and comments.

--- src/main/java/com/invoicegenerator/services/InvoiceGenerator.java
+++ src/main/java/com/invoicegenerator/services/InvoiceGenerator.java
@@ -6,23 +6,46 @@ import com.invoicegenerator.utility.RideRepository;
 import com.invoicegenerator.utility.RideType;
 
 public class InvoiceGenerator {
+    /**
+     * Method to calculate fare.
+     * @param rideType
+     * @param distance
+     * @param time
+     * @return maximum between minimum and totalFare.
+     */
     public double calculateFare(RideType rideType, double distance, int time) {
         double totalFare = distance * rideType.minimumCostPerKM + time * rideType.costPerTime;
         return Math.max(rideType.minimumCostPerKM, totalFare);
     }
 
+    /**
+     * Calculating totalFare for all the rides of user.
+     * @param rides
+     * @return
+     */
     public InvoiceSummary calculateFare(Ride[] rides) {
         double totalFare = 0;
         for (Ride ride : rides) {
             totalFare += this.calculateFare(ride.rideType, ride.distance, ride.time);
         }
+
         return new InvoiceSummary(rides.length, totalFare);
     }
 
+    /**
+     * Method to userID and rides
+     * @param userId
+     * @param rides
+     */
     public void addRide(String userId, Ride[] rides) {
         RideRepository.addRides(userId, rides);
     }
 
+    /**
+     * Method to get invoice summary of the userID.
+     * @param userId
+     * @return
+     */
     public InvoiceSummary getInvoiceSummary(String userId) {
         Ride[] rides = RideRepository.getRides(userId);
         return this.calculateFare(rides);
